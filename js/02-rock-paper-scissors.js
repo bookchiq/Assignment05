55ad5b159f63a9b610e389780bbfa7c9c433be57
Sarah Lewis <sarah@happyhumans.com>
2025-03-09 07:28:58 -0700

    Set up Rock Paper Scissors

---
+++ js/02-rock-paper-scissors.js
@@ -0,0 +1,33 @@
+function rockPaperScissors() {
+	do {
+		var userChoice = prompt("Do you choose rock, paper or scissors?").toLowerCase();
+		if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
+			alert("Invalid choice. Please choose rock, paper or scissors.");
+			continue;
+		}
+
+		var computerChoice = Math.random();
+		if (computerChoice < 0.34) {
+			computerChoice = "rock";
+		} else if(computerChoice <= 0.67) {
+			computerChoice = "paper";
+		} else {
+			computerChoice = "scissors";
+		}
+		alert(`The computer chose ${computerChoice}.` + "\n" + compare(userChoice, computerChoice) );
+	} while (confirm("Do you want to play again?"));
+}
+
+function compare(choice1, choice2) {
+	if (choice1 === choice2) {
+		return "The result is a tie!";
+	} else if (choice1 === "rock") {
+		return choice2 === "scissors" ? "Rock wins." : "Paper wins.";
+	} else if (choice1 === "paper") {
+		return choice2 === "rock" ? "Paper wins." : "Scissors wins.";
+	} else if (choice1 === "scissors") {
+		return choice2 === "rock" ? "Rock wins." : "Scissors wins.";
+	} else {
+		return "Invalid choice. Please choose rock, paper or scissors.";
+	}
+}
